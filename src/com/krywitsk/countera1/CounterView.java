bb5a9b536047dc53b4029b8b4211d3113dacc544
krywitsk <krywitsk@ualberta.ca>
2014-01-17 21:37:12 -0700

    Added counter class for main view 1

---
+++ src/com/krywitsk/countera1/CounterView.java
@@ -0,0 +1,13 @@
+package com.krywitsk.countera1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CounterView extends View {
+
+	public CounterView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
